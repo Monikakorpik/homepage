fbb76e14d0bbb0aafce820f15704061393631800
Monika Korpik <monika.zboralska91@wp.pl>
2023-09-16 22:11:00 +0200

    function added

--- js/script.js
+++ js/script.js
@@ -6,3 +6,11 @@ changeBackgroundcolorButton.addEventListener("click", () => {
   body.classList.toggle("dark");
   themeName.innerText = body.classList.contains(".dark") ? "różowy" : "zielony";
 });
+
+function double(numer){
+  return number *2;
+}
+let myNumber =5;
+
+console.log(double(myNumber));
+console.log(double(10));
