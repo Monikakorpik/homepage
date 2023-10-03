38bf8eab16af40867925e5a4bfcdbd5e95edb4a1
Monika Korpik <monika.zboralska91@wp.pl>
2023-10-03 20:25:51 +0200

    function added

--- js/script.js
+++ js/script.js
@@ -3,14 +3,18 @@
     console.log("Witam wszystkich developerów");
   }
   const onButtonClick = () => {
+    const body = document.querySelector(".body");
     body.classList.toggle("dark");
     themeName.innerText = body.classList.contains(".dark") ? "różowy" : "zielony";
   };
-  welcome();
-  let  changeBackgroundcolorButton = document.querySelector(
-    ".changeBackgroundcolorButton");
-  let body = document.querySelector(".body");
-  let themeName = document.querySelector(".themeName");
-  changeBackgroundcolorButton.addEventListener("click", onButtonClick)();
+  
+  const init = () => {
+    const changeBackgroundcolorButton = document.querySelector(
+      ".changeBackgroundcolorButton");
+    let themeName = document.querySelector(".themeName");
+    changeBackgroundcolorButton.addEventListener("click", onButtonClick)();
+    welcome();
+  };
+  init();
    
 }
